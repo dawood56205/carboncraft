0adcd82d15917d8961d4f183581a0f4226234ac9
dawood56205 <daudqureshi4@gmail.com>
2026-05-14 18:11:07 +0500

    vercel fixed django

--- carboncraftstudio/settings.py
+++ carboncraftstudio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-o6(=#+7k@=jvp)_g=!m&))%=^(b=ax(@nfmf--h1ei^%62uy*#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
